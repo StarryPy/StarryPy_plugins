b47bedcf05fdf0d2842735709b7779b58ba79e07
teihoo <teihoo@gmail.com>
2014-02-08 19:40:41 +0100

    Emotes plugin updates
    Added controls if command is used without <emote>
    Changed emote color to orange

--- plugins/emotes/emotes.py
+++ plugins/emotes/emotes.py
@@ -20,6 +20,13 @@ class EmotesPlugin(SimpleCommandPlugin):
     @permissions(UserLevels.GUEST)
     def me(self, data):
         """Creates a player emote message. Syntax: /me <emote>"""
+        if len(data) == 0:
+            self.protocol.send_chat_message(self.me.__doc__)
+            return
+        if self.protocol.player.muted:
+            self.protocol.send_chat_message(
+                "You are currently muted and cannot emote. You are limited to commands and admin chat (prefix your lines with %s for admin chat." % (self.config.chat_prefix*2))
+            return False
         emote = " ".join(data)
-        self.factory.broadcast_planet("^#CCCCCC;%s %s" % (self.protocol.player.name, emote), planet=self.protocol.player.planet)
+        self.factory.broadcast_planet("^#F49413;%s %s" % (self.protocol.player.name, emote), planet=self.protocol.player.planet)
         return False
