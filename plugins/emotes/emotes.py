503a7dcb8f7af2324cd61165f0bac0eaaa3b1737
teihoo <teihoo@gmail.com>
2014-02-18 19:35:15 +0100

    Emotes update (added predefined emotes)
    Adding predefined emotes:
    /me beckon [Character] beckons
    /me bow [Character] bows
    /me cheer [Character] cheers
    /me cower [Character] cowers
    /me cry [Character] is crying
    /me dance [Character] is busting out some moves, some sweet dance moves
    /me kneel [Character] kneels
    /me laugh [Character] laughs
    /me lol [Character] laughs
    /me no [Character] disagrees
    /me point [Character] points
    /me ponder [Character] ponders
    /me rofl [Character] rolls on the floor laughing
    /me salute [Character] salutes
    /me shrug [Character] shrugs
    /me sit [Character] sits down. Oh, boy...
    /me sleep [Character] falls asleeps. Zzz
    /me surprised [Character] is surprised
    /me threaten [Character] is threatening
    /me wave [Character] waves
    /me yes [Character] agrees

--- plugins/emotes/emotes.py
+++ plugins/emotes/emotes.py
@@ -1,5 +1,3 @@
-# -*- coding: UTF-8 -*-
-
 from base_plugin import SimpleCommandPlugin
 from plugins.core.player_manager import permissions, UserLevels
 
@@ -19,7 +17,7 @@ class EmotesPlugin(SimpleCommandPlugin):
 
     @permissions(UserLevels.GUEST)
     def me(self, data):
-        """Creates a player emote message. Syntax: /me <emote>"""
+        """Creates a player emote message. Syntax: /me <emote>\nPredefined emotes: ^shadow,yellow;beckon^shadow,green;, ^shadow,yellow;bow^shadow,green;, ^shadow,yellow;cheer^shadow,green;, ^shadow,yellow;cower^shadow,green;, ^shadow,yellow;cry^shadow,green;, ^shadow,yellow;dance^shadow,green;, ^shadow,yellow;kneel^shadow,green;, ^shadow,yellow;laugh^shadow,green;, ^shadow,yellow;lol^shadow,green;, ^shadow,yellow;no^shadow,green;, ^shadow,yellow;point^shadow,green;, ^shadow,yellow;ponder^shadow,green;, ^shadow,yellow;rofl^shadow,green;, ^shadow,yellow;salute^shadow,green;, ^shadow,yellow;shrug^shadow,green;, ^shadow,yellow;sit^shadow,green;, ^shadow,yellow;sleep^shadow,green;, ^shadow,yellow;surprised^shadow,green;, ^shadow,yellow;threaten^shadow,green;, ^shadow,yellow;wave^shadow,green;, ^shadow,yellow;yes^shadow,green;"""
         if len(data) == 0:
             self.protocol.send_chat_message(self.me.__doc__)
             return
@@ -28,5 +26,48 @@ class EmotesPlugin(SimpleCommandPlugin):
                 "You are currently muted and cannot emote. You are limited to commands and admin chat (prefix your lines with %s for admin chat." % (self.config.chat_prefix*2))
             return False
         emote = " ".join(data)
-        self.factory.broadcast_planet("^#F49413;%s %s" % (self.protocol.player.name, emote), planet=self.protocol.player.planet)
+        if emote == "beckon":
+            emote = "beckons"
+        elif emote == "bow":
+            emote = "bows"
+        elif emote == "cheer":
+            emote = "cheers"
+        elif emote == "cower":
+            emote = "cowers"
+        elif emote == "cry":
+            emote = "is crying"
+        elif emote == "dance":
+            emote = "is busting out some moves, some sweet dance moves"
+        elif emote == "kneel":
+            emote = "kneels"
+        elif emote == "laugh":
+            emote = "laughs"
+        elif emote == "lol":
+            emote = "laughs"
+        elif emote == "no":
+            emote = "disagrees"
+        elif emote == "point":
+            emote = "points"
+        elif emote == "ponder":
+            emote = "ponders"
+        elif emote == "rofl":
+            emote = "rolls on the floor laughing"
+        elif emote == "salute":
+            emote = "salutes"
+        elif emote == "shrug":
+            emote = "shrugs"
+        elif emote == "sit":
+            emote = "sits down. Oh, boy..."
+        elif emote == "sleep":
+            emote = "falls asleep. Zzz"
+        elif emote == "surprised":
+            emote = "is surprised"
+        elif emote == "threaten":
+            emote = "is threatening"
+        elif emote == "wave":
+            emote = "waves"
+        elif emote == "yes":
+            emote = "agrees"
+
+        self.factory.broadcast_planet("^shadow,orange;%s %s" % (self.protocol.player.name, emote), planet=self.protocol.player.planet)
         return False
